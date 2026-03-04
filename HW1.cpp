7cf3ada890cdf81ccab893749dd60bea12b2a428
yuklya <yuloseva@mail.ru>
2026-03-04 08:23:56 +0300

    realize interface

--- HW1.cpp
+++ HW1.cpp
@@ -1,3 +1,16 @@
-int main()
-{
+template<class T, class Compare = std::less<T>>
+T* merge(const T* a, size_t sa,
+         const T* b, size_t sb,
+         T* c, Compare comp = {}) {
+    size_t i = 0, j = 0, k = 0;
+    while (i < sa && j < sb) {
+        if (comp(a[i], b[j])) {
+            c[k++] = a[i++];
+        } else {
+            c[k++] = b[j++];
+        }
+    }
+    while (i < sa) c[k++] = a[i++];
+    while (j < sb) c[k++] = b[j++];
+    return c;
 }
