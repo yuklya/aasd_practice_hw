d0a28ac571cb04712fc24573e26782f6e65b0159
yuklya <yuloseva@mail.ru>
2026-03-04 08:27:26 +0300

    fix realize

--- HW1.cpp
+++ HW1.cpp
@@ -1,10 +1,9 @@
-template<class T, class Compare = std::less<T>>
-T* merge(const T* a, size_t sa,
-         const T* b, size_t sb,
-         T* c, Compare comp = {}) {
+//var 2
+template<class T>
+T* merge(const T* a, size_t sa, const T* b, size_t sb, T* c) {
     size_t i = 0, j = 0, k = 0;
     while (i < sa && j < sb) {
-        if (comp(a[i], b[j])) {
+        if (a[i], b[j]) {
             c[k++] = a[i++];
         } else {
             c[k++] = b[j++];
